762cdf5a0a6b6706693a99e535364a87af7df30b
Angèle Henry <angele.henry@outlook.com>
2024-09-26 23:24:42 +0200

    overflow-scrolling: touch

--- src/components/CarouselCards.vue
+++ src/components/CarouselCards.vue
@@ -5,10 +5,11 @@ const props = defineProps(['skills'])
 
 <template>
   <div class="carousel-container">
-    <div v-for="(item, index) in props.skills" :key="index">
-      <h2>{{ item.type }}</h2>
-      <div class="cards-line">
-        <div v-for="(child, j) in item.skills" :key="j">{{ child.title }}</div>
+    <div v-for="(item, index) in props.skills" :key="index" class="cards-line">
+      <div v-for="(child, j) in item.skills" :key="j" class="card-item">
+        <div class="title">
+          {{ child.title }}
+        </div>
       </div>
     </div>
   </div>
@@ -16,14 +17,36 @@ const props = defineProps(['skills'])
 
 <style scoped>
 .carousel-container {
+  --card-height: 150px;
+  --gap: 48px;
+  overflow-x: hidden;
+  position: relative;
   display: flex;
   flex-direction: column;
-  gap: 50px;
+  gap: var(--gap);
   padding: 20px;
-  background-color: var(--vt-c-black-mute);
+  height: calc((var(--card-height) * 3) + (var(--gap) * 2) + 90px);
 }
 .cards-line {
+  width: 100%;
   display: flex;
   flex-direction: row;
+  gap: var(--gap);
+  overflow-x: auto;
+  -webkit-overflow-scrolling: touch;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+.card-item {
+  padding: 10px;
+  background-color: var(--vt-c-black-mute);
+  border: 1px solid var(--color-border);
+  border-radius: 15px;
+  min-width: 200px;
+  max-width: 200px;
+  height: var(--card-height);
+  flex: 0 0 auto;
 }
 </style>
