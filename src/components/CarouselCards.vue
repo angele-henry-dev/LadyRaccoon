852cbfb781ed56d1dc00a1faab2c3b93d79f5ec0
Angèle Henry <angele.henry@outlook.com>
2024-09-27 12:15:57 +0200

    Cards system?

--- src/components/CarouselCards.vue
+++ src/components/CarouselCards.vue
@@ -4,49 +4,97 @@ const props = defineProps(['skills'])
 </script>
 
 <template>
-  <div class="carousel-container">
-    <div v-for="(item, index) in props.skills" :key="index" class="cards-line">
-      <div v-for="(child, j) in item.skills" :key="j" class="card-item">
-        <div class="title">
-          {{ child.title }}
-        </div>
+  <div class="cards-line">
+    <div
+      v-for="(item, index) in props.skills"
+      :key="index"
+      :class="`card-item${index === 0 ? ' card-active' : ''}`"
+    >
+      <div class="card-frame"></div>
+      <div class="card-inner">
+        {{ item.type }}
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.carousel-container {
-  --card-height: 150px;
-  --gap: 48px;
-  overflow-x: hidden;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  gap: var(--gap);
-  padding: 20px;
-  height: calc((var(--card-height) * 3) + (var(--gap) * 2) + 90px);
-}
 .cards-line {
+  /** 315 par 420 mobile */
+  --gap: 10px;
+  margin-bottom: 50px;
   width: 100%;
   display: flex;
   flex-direction: row;
-  gap: var(--gap);
+  flex-wrap: nowrap;
   overflow-x: auto;
   -webkit-overflow-scrolling: touch;
   &::-webkit-scrollbar {
     display: none;
   }
 }
+@media (width > 48em) {
+  .cards-line {
+    --card-width: 360px;
+  }
+}
+@media (width > 48em) {
+  .cards-line {
+    --card-height: 480px;
+  }
+}
+@media (width > 20.3125em) {
+  .cards-line {
+    --card-width: 315px;
+  }
+}
+@media (width < 47.99em) {
+  .cards-line {
+    --card-height: 420px;
+  }
+}
 
 .card-item {
-  padding: 10px;
+  position: relative;
+  width: var(--card-width);
+  height: var(--card-height);
+  flex: 0 0 auto;
+}
+.card-frame {
+  -webkit-transform: scale(0.9445, 0.7625);
+  transform: scale(0.9445, 0.7625);
+  transition:
+    background 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94),
+    -webkit-transform 0.6s cubic-bezier(0.19, 1, 0.22, 1);
+  background-color: #fff;
+}
+.card-frame,
+.card-frame:after,
+.card-frame:before {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
   background-color: var(--vt-c-black-mute);
   border: 1px solid var(--color-border);
   border-radius: 15px;
-  min-width: 200px;
-  max-width: 200px;
-  height: var(--card-height);
-  flex: 0 0 auto;
+}
+.card-active .card-frame {
+  -webkit-transform: scale(1);
+  transform: scale(1);
+  background-color: var(--vt-c-white-mute);
+}
+
+.card-inner {
+  height: 100%;
+  width: 100%;
+  padding: 57px 10px;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
 }
 </style>
