90efd064d367ed60547b622702c2c68874bbae48
Angèle Henry <angele.henry@outlook.com>
2024-11-07 21:41:21 +0100

    Shooting star

--- src/components/StarsBackground.vue
+++ src/components/StarsBackground.vue
@@ -27,7 +27,10 @@ const stars = ref([
 </script>
 
 <template>
-  <div class="retrobg-stars">
+  <div ref="container" class="retrobg-stars">
+    <div class="shooting-star">
+      <div v-for="x in 40" :key="x" class="shooting-star-tail"></div>
+    </div>
     <div
       v-for="(star, index) in stars"
       :key="index"
@@ -65,5 +68,218 @@ const stars = ref([
   50% {
     opacity: 0.2;
   }
+}
+
+.shooting-star {
+  position: absolute;
+  top: 50%;
+  left: -20px;
+  width: 1px;
+  height: 1px;
+  background-color: #fff;
+  opacity: 0.6;
+  box-shadow:
+    0 0 2px 1px #fff,
+    0 0 3px 2px #fff;
+  opacity: 1;
+  animation: shooting-star-move 2s linear;
+  animation-delay: 1s;
+}
+@keyframes shooting-star-move {
+  0% {
+    opacity: 1;
+    transform: translateX(-20px) translateY(-200px) rotate(70deg);
+  }
+  100% {
+    opacity: 0;
+    transform: translateX(1000px) translateY(-600px) rotate(70deg);
+  }
+}
+
+@keyframes tail-move {
+  0% {
+    opacity: 0;
+    box-shadow: none;
+  }
+  25% {
+    opacity: 0.5;
+    box-shadow:
+      0 0 1px 0.5px #fff,
+      0 0 1.5px 1px #fff;
+  }
+  100% {
+    opacity: 0;
+    box-shadow: none;
+    height: 0;
+    width: 0;
+    transform: translateY(180px);
+  }
+}
+
+.shooting-star .shooting-star-tail {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  background: white;
+}
+.shooting-star .shooting-star-tail:nth-child(1) {
+  left: -3px;
+  animation: 2s tail-move -380ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(2) {
+  left: -2px;
+  animation: 2s tail-move -566ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(3) {
+  left: -3px;
+  animation: 2s tail-move -1456ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(4) {
+  left: 2px;
+  animation: 2s tail-move -18ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(5) {
+  left: 2px;
+  animation: 2s tail-move -1655ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(6) {
+  left: -1px;
+  animation: 2s tail-move -1556ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(7) {
+  left: 2px;
+  animation: 2s tail-move -1286ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(8) {
+  left: 2px;
+  animation: 2s tail-move -1431ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(9) {
+  left: 0px;
+  animation: 2s tail-move -210ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(10) {
+  left: 1px;
+  animation: 2s tail-move -1709ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(11) {
+  left: -3px;
+  animation: 2s tail-move -1225ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(12) {
+  left: 0px;
+  animation: 2s tail-move -1495ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(13) {
+  left: -1px;
+  animation: 2s tail-move -1257ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(14) {
+  left: 1px;
+  animation: 2s tail-move -647ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(15) {
+  left: 3px;
+  animation: 2s tail-move -1429ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(16) {
+  left: 1px;
+  animation: 2s tail-move -704ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(17) {
+  left: -2px;
+  animation: 2s tail-move -859ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(18) {
+  left: 1px;
+  animation: 2s tail-move -183ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(19) {
+  left: 3px;
+  animation: 2s tail-move -541ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(20) {
+  left: -2px;
+  animation: 2s tail-move -1022ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(21) {
+  left: 2px;
+  animation: 2s tail-move -1014ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(22) {
+  left: -1px;
+  animation: 2s tail-move -1785ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(23) {
+  left: -1px;
+  animation: 2s tail-move -197ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(24) {
+  left: 2px;
+  animation: 2s tail-move -64ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(25) {
+  left: 2px;
+  animation: 2s tail-move -1295ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(26) {
+  left: 1px;
+  animation: 2s tail-move -191ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(27) {
+  left: -1px;
+  animation: 2s tail-move -1640ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(28) {
+  left: 2px;
+  animation: 2s tail-move -1291ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(29) {
+  left: -3px;
+  animation: 2s tail-move -1148ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(30) {
+  left: -1px;
+  animation: 2s tail-move -1472ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(31) {
+  left: 1px;
+  animation: 2s tail-move -990ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(32) {
+  left: 0px;
+  animation: 2s tail-move -1436ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(33) {
+  left: 1px;
+  animation: 2s tail-move -691ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(34) {
+  left: 2px;
+  animation: 2s tail-move -1761ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(35) {
+  left: 3px;
+  animation: 2s tail-move -868ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(36) {
+  left: 0px;
+  animation: 2s tail-move -1498ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(37) {
+  left: -1px;
+  animation: 2s tail-move -1343ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(38) {
+  left: 0px;
+  animation: 2s tail-move -1728ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(39) {
+  left: 3px;
+  animation: 2s tail-move -1876ms ease-in infinite;
+}
+.shooting-star .shooting-star-tail:nth-child(40) {
+  left: -3px;
+  animation: 2s tail-move -967ms ease-in infinite;
 }
 </style>
